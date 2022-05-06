a3cd74dc4a702bd1f090db6c17b4cc87aadb30b7
hector <hpulido1021@gmail.com>
2022-05-05 19:42:40 -0700

    Completed assignment

--- main.js
+++ main.js
@@ -3,26 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-const init = () => {
-  const inputForm = document.querySelector('article');
-  inputForm.addEventListener('click', (event) => {
-    event.preventDefault();
-    const input = document.querySelector('input#searchByID');
-    fetch(mimicServerCall)
-    .then(response => response.json())
-    .catch (error => {
-      alert("Wrong name! Please try again!");
-      console.log(error.message);
-      document.body.append(error.message)
-    });
-    
-    // .then(data => {
-    //   const title = document.querySelector('section#movieDetails h4');
-    //   const summary = document.querySelector('section#movieDetails p');
-    //   title.innerText = data.title;
-    //   summary.innerText = data.summary;
-    // });
-  });
+let heartIcons = document.querySelectorAll(".like-glyph")
+// console.log(heartIcons)
+
+heartIcons.forEach(item => {
+  item.addEventListener('click', onClick)
+})
+
+function onClick(e) {
+
+  mimicServerCall()
+  .then(data => {
+    if(data && e.target.innerHTML === EMPTY_HEART){
+      e.target.className = 'activated-heart'
+      e.target.innerHTML = FULL_HEART
+    }else if (data || e.target.innerHTML === FULL_HEART){
+      e.target.className = 'like-glyph'
+      e.target.innerHTML = EMPTY_HEART
+    }
+  })
+  .catch(function(error){
+    const modal = document.querySelector('div#modal')
+    modal.className = ''
+    const p = document.querySelector(['p#modal-message'])
+    p.innerHTML = error
+    setTimeout(function () {
+      return modal.className = 'hidden'
+    }, 3000)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
